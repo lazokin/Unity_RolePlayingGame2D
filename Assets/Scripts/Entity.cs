198ad06fcea9d4a75eaa314b99b0f617541781ef
Nikolce Ambukovski <lazokin@hotmail.com>
2016-04-08 18:02:12 +1000

    Planning behaviors

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,7 +4,7 @@ public class Entity : ScriptableObject {
 
 	public string Name;
 	public int Age;
-	string Faction;
+	public string Faction;
 	public string Occupation;
 	public int Level = 1;
 	public int Health = 2;
@@ -17,5 +17,13 @@ public class Entity : ScriptableObject {
 	public int NoOfAttacks = 1;
 	public string Weapon;
 	public Vector2 Position;
+
+	public void TakeDamage(int Amount) {
+		Health - Mathf.Clamp (Amount - Armor, 0, int.MaxValue);
+	}
+
+	public void Attack(Entity Entity) {
+		Entity.TakeDamage(Strength);
+	}
 
 }
